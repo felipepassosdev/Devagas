6aa7f87125ce0b7972bed3448349dc5a4a48f0ba
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-09-16 18:31:17 -0300

    Create styles.ts

---
+++ src/Header/styles.ts
@@ -0,0 +1,13 @@
+
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 50px 0;
+
+    h1 {
+        color: #fff
+    }
+`;
