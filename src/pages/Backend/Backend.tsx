ed921f0b4acf2b7daf50e2e68122c538a17b95df
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-09-16 18:31:25 -0300

    Create Backend.tsx

---
+++ src/pages/Backend/Backend.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../../services/api'
+import { Cards, Title } from './styles';
+
+// import { Container } from './styles';
+
+interface Repository {
+    html_url: string,
+    body: string,
+    title: string,
+    labels: Array<any>,
+    state: string
+}
+
+
+const Backend: React.FC = () => {
+    const [data, setData] = useState<Repository[]>([])
+
+    const getApi = async () => {
+        await axios.get(`https://api.github.com/repos/backend-br/vagas/issues`)
+            .then(function (response: any) {
+                console.log(response.data)
+                setData(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getApi()
+    }, [])
+
+    return (
+        <>
+            <Title>Vagas: Backend</Title>
+            <Cards>
+                {data.map((repo, index) => (
+                    <li key={index}>
+                        {/* <h2>{repo.state ? "Aberta" : "Fechado"}</h2> */}
+                        <strong><a href={repo.html_url} target="_blank">{repo.title}</a></strong>
+
+                        <div>{repo.labels.map(label => (
+                            <div key={label.name}>
+                                <p> #{label.name} </p>
+                            </div>
+                        ))}</div>
+                    </li>
+                ))}
+            </Cards>
+        </>
+    );
+}
+
+export default Backend;
