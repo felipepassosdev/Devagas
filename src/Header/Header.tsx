90ca29577535799f75ddab7a7f8c62d6601b7d3b
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-09-16 18:31:08 -0300

    header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link  } from 'react-router-dom'
+
+import { Container } from './styles';
+
+function Header() {
+  return (
+    <Container>
+     <h1>Devagas</h1>
+    </Container>
+  );
+}
+
+export default Header;
