f843b54d86ff197b3629f01b2a003ac3699f56d9
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-09-17 12:03:59 -0300

    Update Header.tsx

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-// import { Link  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Container } from './styles';
 
 function Header() {
   return (
     <Container>
-     <h1>Devagas</h1>
+      <nav>
+        <span>Devagas</span>
+      </nav>
+      <ul>
+        <Link to="/Frontend"><li>Frontend</li></Link>
+        <Link to="/Backend"><li>Backend</li></Link>
+        <Link to="/Mobile"><li>Mobile</li></Link>
+        <Link to="/Design"><li>Design</li></Link>
+      </ul>
     </Container>
   );
 }
